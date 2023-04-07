4b01d5e132a644828b4cb7702320178662666f34
Yuda Xwer <81358285+YudaXwer@users.noreply.github.com>
2023-04-07 10:15:21 +0700

    Create firebaseConfig.js

---
+++ src/firebaseConfig.js
@@ -0,0 +1,17 @@
+const firebaseConfig = {
+
+  apiKey: "AIzaSyAln_3zsGB7TASFU8f5tM5_G4XM4WaLWkM",
+
+  authDomain: "animeyx-acf71.firebaseapp.com",
+
+  projectId: "animeyx-acf71",
+
+  storageBucket: "animeyx-acf71.appspot.com",
+
+  messagingSenderId: "497323073940",
+
+  appId: "1:497323073940:web:cdf13318bbd9d1e923af81",
+
+  measurementId: "G-D5YD3P93KQ"
+
+};
